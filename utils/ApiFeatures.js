5c4bc888475f074ac7df811368e670eaad82b7bd
Danushkapramod <k.danushkapramod@gmail.com>
2024-07-30 04:43:52 +0530

    add

--- utils/ApiFeatures.js
+++ utils/ApiFeatures.js
@@ -6,7 +6,7 @@ export class ApiFeatures {
 
     filtering() {
         const queryObj = { ...this.req.query }
-        const excludField = ['page', 'sort', 'limit', 'fields', 'teacher','search']
+        const excludField = ['page', 'sort', 'limit', 'fields', 'teacher','search','field']
         excludField.forEach((el) => {
             delete queryObj[el]
         })
@@ -38,7 +38,7 @@ export class ApiFeatures {
 
     searching() {
         if (this.req.query.search) {
-            const query = { name: { $regex: this.req.query.search, $options: "i" } };
+            const query = { [this.req.query.field || 'name']: { $regex: this.req.query.search, $options: "i" } };
             this.query = this.query.find(query)
         }
         return this
